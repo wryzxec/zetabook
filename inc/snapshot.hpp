10a157233557ebefd85b3b3e3e06e8bec544b90f
wryzxec <lucas.bruckbauer@gmail.com>
2025-06-29 23:56:33 +0100

    chore(snapshot): neater snapshot output format

--- inc/snapshot.hpp
+++ inc/snapshot.hpp
@@ -1,37 +1,41 @@
 #pragma once
 
+#include <cassert>
 #include <cstddef>
 #include <cstdint>
 #include <iomanip>
+#include <ios>
 #include <sstream>
 #include <string>
 #include <vector>
+
 struct Snapshot {
     std::vector<std::pair<uint64_t, size_t>> raw_asks;
     std::vector<std::pair<uint64_t, size_t>> raw_bids;
 
-    size_t precision = 4;
+    // formatting constants
+    static constexpr int precision = 3;
+    static constexpr int col_spacing = 20;
 
     std::string to_string() const {
         std::ostringstream oss;
         oss << std::fixed << std::setprecision(precision);
+        oss << std::left;
 
-        oss << "Asks: ";
-        for(int i = 0; i < raw_asks.size(); ++i) {
-            double price = static_cast<double>(raw_asks[i].first) / 10000.0;
-            oss << price << "x" << raw_asks[i].second;
-            if(i + 1 < raw_asks.size()) {
-                oss << ", ";
-            }
-        }
+        assert(raw_asks.size() == raw_bids.size());
+        size_t asks_size = raw_asks.size();
+        
+        oss << std::setw(col_spacing) << "Size";
+        oss << std::setw(col_spacing) << "Price";
+        oss << std::setw(col_spacing) << "Price";
+        oss << std::setw(col_spacing) << "Size" << "\n";
 
-        oss << " | bids: ";
-        for(int i = 0; i < raw_bids.size(); ++i) {
-            double price = static_cast<double>(raw_bids[i].first) / 10000.0;
-            oss << price << "x" << raw_bids[i].second;
-            if(i + 1 < raw_bids.size()) {
-                oss << ", ";
-            }
+        for(int i = 0; i < asks_size ; ++i) {
+            oss << std::setw(col_spacing) << raw_asks[i].second;
+            oss << std::setw(col_spacing) << "£" + std::to_string(raw_asks[i].first / 10000.0);
+            oss << std::setw(col_spacing) << "£" + std::to_string(raw_bids[i].first / 10000.0);
+            oss << std::setw(col_spacing) << raw_bids[i].second;
+            oss << std::endl;
         }
 
         return oss.str();
